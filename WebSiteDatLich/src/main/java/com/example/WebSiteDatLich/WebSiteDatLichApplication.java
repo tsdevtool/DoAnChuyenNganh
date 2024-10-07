bb67abc741512ed8442faa42a72263f4c96c8e7c
ducteo2003 <103983920+ducteo2003@users.noreply.github.com>
2024-10-07 19:25:22 +0700

    ComitWeb

---
+++ WebSiteDatLich/src/main/java/com/example/WebSiteDatLich/WebSiteDatLichApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebSiteDatLich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSiteDatLichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSiteDatLichApplication.class, args);
+	}
+
+}
---
+++ WebSiteDatLich/src/test/java/com/example/WebSiteDatLich/WebSiteDatLichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebSiteDatLich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteDatLichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
